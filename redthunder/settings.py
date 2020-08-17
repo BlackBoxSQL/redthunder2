d5110bc24bdba2425089a2851cb85986508131d3
BlackBoxSQL <pythonboy007@gmail.com>
2020-08-18 03:30:17 +0600

    debug true

--- redthunder/settings.py
+++ redthunder/settings.py
@@ -24,8 +24,8 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = config('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = config('DEBUG', default=False, cast=bool)
-
+# DEBUG = config('DEBUG', default=False, cast=bool)
+DEBUG = True
 ALLOWED_HOSTS = ['redthunderfriendsclub.herokuapp.com']
 
 
